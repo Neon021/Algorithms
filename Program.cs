0149d938a36a030c0677d6c99a6669f9d0d5c98b
Furkan Bilal <bilalyigit594@gmail.com>
2023-11-16 17:53:07 +0300

    Binary Search algorithm

--- Program.cs
+++ Program.cs
@@ -2,5 +2,28 @@
 {
     public static void Main(string[] args)
     {
+        int[] haystack = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 };
+        Console.WriteLine(BinarySearch(haystack, 10));
+    }
+
+    public static bool BinarySearch(int[] haystack, int needle)
+    {
+        int low = 0;
+        int high = haystack.Length;
+        do
+        {
+            int index = low + ((high - low) / 2);
+            int value = haystack[index];
+
+            if (value == needle)
+                return true;
+            else if(value > needle) 
+                high = index;
+            else
+                low = index + 1;
+
+        } while (low < high);
+
+        return false;
     }
 }
