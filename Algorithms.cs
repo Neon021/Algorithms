ee0fcc10d7b6eef8a8234a5101cedb98ae6c34fd
Furkan Bilal <bilalyigit594@gmail.com>
2023-11-21 18:07:31 +0300

    Stack data structure

--- Algorithms.cs
+++ Algorithms.cs
@@ -220,6 +220,62 @@ public class Algorithms
     }
 
 
+    //Queue
+    public class Queue<T>
+    {
+        public int Length { get; internal set; }
+        private Node<T>? Head { get; set; }
+        private Node<T>? Tail { get; set; }
+
+        public Queue()
+        {
+            this.Head = this.Tail = null;
+            this.Length = 0;
+        }
+
+        public void Enqueue(T data)
+        {
+            Node<T> node = new(data);
+            this.Length++;
+            if (this.Tail is null)
+            {
+                this.Tail = this.Head = node;
+                return;
+            }
+
+            this.Tail.Next = node;
+            this.Tail = node;
+        }
+        public T? Deque<T>() where T : class
+        {
+            if (this.Head == null)
+            {
+                return null;
+            }
+
+            this.Length--;
+
+            var head = this.Head;
+            this.Head = this.Head.Next;
+
+            // free
+            head.Next = null;
+
+            if (this.Length == 0)
+            {
+                this.Tail = null;
+            }
+
+            return head.Data as T;
+        }
+
+        public T? Peek<T>() where T : class
+        {
+            return this.Head is not null ? this.Head.Data as T : null;
+        }
+    }
+
+
     //////////////////////////////////////////////////////////////////////////////////
     public static bool IsPangram(string input)
     {
--- Program.cs
+++ Program.cs
@@ -10,59 +10,55 @@ public class Codewars
     public class Node<T>
     {
         public T Data { get; set; }
-        public Node<T> Next { get; internal set; }
+        public Node<T>? Prev { get; internal set; }
 
         public Node(T data)
         {
             this.Data = data;
         }
     }
-    public class Queue<T>
-    {
-        public int Length { get; internal set; }
-        private Node<T>? Head { get; set; }
-        private Node<T>? Tail { get; set; }
 
-        public Queue()
+    public class Stack<T>
+    {
+        public int Length;
+        private Node<T>? Head;
+
+        public Stack()
         {
-            this.Head = this.Tail = null;
+            this.Head = null;
             this.Length = 0;
         }
 
-        public void Enqueue(T data)
+        public void Push(T data)
         {
             Node<T> node = new(data);
             this.Length++;
-            if (this.Tail is null)
+
+            if (this.Head is null)
             {
-                this.Tail = this.Head = node;
+                this.Head = node;
                 return;
             }
 
-            this.Tail.Next = node;
-            this.Tail = node;
+            node.Prev = this.Head;
+            this.Head = node;
         }
-        public T? Deque<T>() where T : class
+
+        public T? Pop() 
         {
-            if (this.Head == null)
-            {
-                return null;
-            }
-
-            this.Length--;
-
-            var head = this.Head;
-            this.Head = this.Head.Next;
-
-            // free
-            head.Next = null;
+            this.Length = Math.Max(0, this.Length - 1);
 
             if (this.Length == 0)
             {
-                this.Tail = null;
+                Node<T>? head0 = this.Head as Node<T>;
+                this.Head = null;
+                return head0.Data;
             }
 
-            return head.Data as T;
+            Node<T>? head = this.Head as Node<T>;
+            this.Head = head.Prev;
+
+            return head.Data;
         }
 
         public T? Peek<T>() where T : class
