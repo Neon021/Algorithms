22d9b55ca1ed20818f510b2c19f50656f1e7c519
Furkan Bilal <bilalyigit594@gmail.com>
2023-11-23 17:40:43 +0300

    ArrayList<T> data structure implemented

--- Algorithms.cs
+++ Algorithms.cs
@@ -94,6 +94,7 @@ public class Algorithms
     {
         public T Data { get; set; }
         public Node<T> Next { get; internal set; }
+        public Node<T>? Prev { get; internal set; }
 
         public Node(T data)
         {
@@ -276,7 +277,84 @@ public class Algorithms
     }
 
 
+//Stack
+
+    public class Stack<T>
+    {
+        public int Length;
+        private Node<T>? Head;
+
+        public Stack()
+        {
+            this.Head = null;
+            this.Length = 0;
+        }
+
+        public void Push(T data)
+        {
+            Node<T> node = new(data);
+            this.Length++;
+
+            if (this.Head is null)
+            {
+                this.Head = node;
+                return;
+            }
+
+            node.Prev = this.Head;
+            this.Head = node;
+        }
+
+        public T? Pop()
+        {
+            this.Length = Math.Max(0, this.Length - 1);
+
+            if (this.Length == 0)
+            {
+                Node<T>? head0 = this.Head as Node<T>;
+                this.Head = null;
+                return head0.Data;
+            }
+
+            Node<T>? head = this.Head as Node<T>;
+            this.Head = head.Prev;
+
+            return head.Data;
+        }
+
+        public T? Peek<T>() where T : class
+        {
+            return this.Head is not null ? this.Head.Data as T : null;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
     //////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public static bool IsPangram(string input)
     {
         string alphabet = "abcdefghijklmnopqrstuvwxyz";
--- Program.cs
+++ Program.cs
@@ -1,69 +1,95 @@
-﻿using System.ComponentModel;
-
-public class Codewars
+﻿public class Codewars
 {
     public static void Main(string[] args)
     {
-
+        ArrayList<int> arrayList = new(3);
+        arrayList.Push(1);
+        arrayList.Push(2);
+        arrayList.Push(3);
+        arrayList.Push(4);
+        arrayList.Push(5);
+        arrayList.Push(6);
+        arrayList.Enqueue(7);
+        arrayList.Dequeue();
+        Console.WriteLine(arrayList.Get(0));
+        Console.WriteLine(arrayList.Get(1));
+        Console.WriteLine(arrayList.Get(2));
+        Console.WriteLine(arrayList.Get(3));
+        Console.WriteLine(arrayList.Get(4));
+        Console.WriteLine(arrayList.Get(5));
+        Console.WriteLine(arrayList.Get(6));
     }
 
-    public class Node<T>
+    public class ArrayList<T>
     {
-        public T Data { get; set; }
-        public Node<T>? Prev { get; internal set; }
+        public int Length { get; set; } = 0;
+        public int Capacity { get; set; }
+        private T[] Array { get; set; }
 
-        public Node(T data)
+        public ArrayList(int Capacity)
         {
-            this.Data = data;
-        }
-    }
-
-    public class Stack<T>
-    {
-        public int Length;
-        private Node<T>? Head;
-
-        public Stack()
-        {
-            this.Head = null;
-            this.Length = 0;
+            this.Capacity = Capacity;
+            Array = new T[Capacity];
         }
 
-        public void Push(T data)
-        {
-            Node<T> node = new(data);
-            this.Length++;
+        //index <= length - 1???
+        public T? Get(int index) => (Length > 0) && (index >= 0 && index < Length) ? Array[index] : default(T);
 
-            if (this.Head is null)
+        public void Push(T value)
+        {
+            if (Length + 1 <= Capacity)
             {
-                this.Head = node;
-                return;
+                this.Array[Length] = value;
+                Length++;
             }
-
-            node.Prev = this.Head;
-            this.Head = node;
-        }
-
-        public T? Pop() 
-        {
-            this.Length = Math.Max(0, this.Length - 1);
-
-            if (this.Length == 0)
+            else
             {
-                Node<T>? head0 = this.Head as Node<T>;
-                this.Head = null;
-                return head0.Data;
+                T[] newArray = new T[Capacity * 2];
+                for (int i = 0; i < this.Length; i++)
+                {
+                    newArray[i] = this.Array[i];
+                }
+                Capacity *= 2;
+                newArray[Length] = value;
+                Length += 1;
+                this.Array = newArray;
             }
-
-            Node<T>? head = this.Head as Node<T>;
-            this.Head = head.Prev;
-
-            return head.Data;
         }
 
-        public T? Peek<T>() where T : class
+        public T? Pop() => Length > 0 ? this.Array[Length - 1] : default(T);
+
+        public void Enqueue(T value)
         {
-            return this.Head is not null ? this.Head.Data as T : null;
+            if (Length + 1 <= Capacity)
+            {
+                for (int i = Length - 1; i >= 0; i--)
+                {
+                    this.Array[i + 1] = this.Array[i];
+                }
+                this.Array[0] = value;
+            }
+            else
+            {
+                T[] newArray = new T[Capacity * 2];
+                for (int i = Length - 1; i >= 0; i--)
+                {
+                    newArray[i + 1] = this.Array[i];
+                }
+                Capacity *= 2;
+                newArray[0] = value;
+                Length += 1;
+                this.Array = newArray;
+            }
+        }
+
+        public void Dequeue()
+        {
+            for (int i = 0; i < Length - 1; i++)
+            {
+                this.Array[i] = this.Array[i + 1];
+            }
+            Length--;
+            this.Array[Length] = default(T);
         }
     }
 }
