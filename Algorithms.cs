63f6b7fe5f2b95096d0eb0311e0cbb38de97ac50
Adrian Yigit <bilalyigit594@gmail.com>
2024-02-23 18:16:35 +0300

    TwoSum algorithm; 23/02/2024; think simpler

--- Algorithms.cs
+++ Algorithms.cs
@@ -1630,6 +1630,27 @@ public class Algorithms
         return -1;
     }
     #endregion
+    #region ProductExceptSelf
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        List<int> answer = new List<int>();
+
+        foreach (int i in nums)
+        {
+            List<int> list = new List<int> { i };
+            var remainingElements = nums.Except(list);
+
+            int product = 1;
+            foreach (int element in remainingElements)
+            {
+                product *= element;
+            }
+
+            answer.Add(product);
+        }
+        return answer.ToArray();
+    }
+    #endregion
 
     #endregion
     #endregion
--- Program.cs
+++ Program.cs
@@ -1,25 +1,33 @@
-﻿public class Codewars
+﻿using System;
+
+public class Codewars
 {
     public static void Main(string[] args)
     {
+        TwoSum(new int[] { 0, 0, 3, 4 }, 0);
     }
-    public int[] ProductExceptSelf(int[] nums)
+    //Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number.
+    //Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+    //Return the indices of the two numbers, index1 and index2, added by one as an integer array[index1, index2] of length 2.
+    public static int[] TwoSum(int[] numbers, int target)
     {
-        List<int> answer = new List<int>();
-
-        foreach (int i in nums)
+        if (target != 0)
         {
-            List<int> list = new List<int> { i };
-            var remainingElements = nums.Except(list);
-
-            int product = 1;
-            foreach (int element in remainingElements)
+            int[] remaining = numbers.Where(x => x <= target || x == 0).ToArray();
+            for (int i = 0, j = remaining.Length - 1; i <= j; i++, j--)
             {
-                product *= element;
+                if (remaining[i] + remaining[j] == target)
+                    return new int[2] { i + 1, j + 1 };
             }
-
-            answer.Add(product);
         }
-        return answer.ToArray();
+        else
+        {
+            int[] remaining = new int[numbers.Length];
+
+            remaining = numbers;
+        }
+
+
+        return Array.Empty<int>();
     }
 }
