86400e5eface4a09ed62bb4b1175613e5c1c3369
Adrian Yigit <bilalyigit594@gmail.com>
2024-02-26 15:50:35 +0300

    MaxProfit ; 26/02/2024

--- Algorithms.cs
+++ Algorithms.cs
@@ -1651,7 +1651,32 @@ public class Algorithms
         return answer.ToArray();
     }
     #endregion
+    #region TwoSumII
+    //Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number.
+    //Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+    //Return the indices of the two numbers, index1 and index2, added by one as an integer array[index1, index2] of length 2.
+    public static int[] TwoSum(int[] numbers, int target)
+    {
+        if (target != 0)
+        {
+            int[] remaining = numbers.Where(x => x <= target || x == 0).ToArray();
+            for (int i = 0, j = remaining.Length - 1; i <= j; i++, j--)
+            {
+                if (remaining[i] + remaining[j] == target)
+                    return new int[2] { i + 1, j + 1 };
+            }
+        }
+        else
+        {
+            int[] remaining = new int[numbers.Length];
 
+            remaining = numbers;
+        }
+
+
+        return Array.Empty<int>();
+    }
+    #endregion
     #endregion
     #endregion
 }
--- Program.cs
+++ Program.cs
@@ -3,31 +3,40 @@
 public class Codewars
 {
     public static void Main(string[] args)
+
     {
-        TwoSum(new int[] { 0, 0, 3, 4 }, 0);
+        int[] prices = { 7, 1, 5, 3, 6, 4 };
+        int resıu = MaxProfit(prices);
     }
-    //Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number.
-    //Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
-    //Return the indices of the two numbers, index1 and index2, added by one as an integer array[index1, index2] of length 2.
-    public static int[] TwoSum(int[] numbers, int target)
+    public static int MaxProfit(int[] prices)
     {
-        if (target != 0)
-        {
-            int[] remaining = numbers.Where(x => x <= target || x == 0).ToArray();
-            for (int i = 0, j = remaining.Length - 1; i <= j; i++, j--)
-            {
-                if (remaining[i] + remaining[j] == target)
-                    return new int[2] { i + 1, j + 1 };
-            }
-        }
-        else
-        {
-            int[] remaining = new int[numbers.Length];
+        int minPrice = prices[0];
+        int maxProfit = 0;
 
-            remaining = numbers;
+        for (int i = 1; i < prices.Length; i++)
+        {
+            minPrice = Math.Min(minPrice, prices[i]);
+            maxProfit = Math.Max(maxProfit, prices[i] - minPrice);
         }
 
+        return maxProfit;
+        //int result = 0;
+        //int i = 0;
+        //int j = 1;
 
-        return Array.Empty<int>();
+        //while (j < prices.Length)
+        //{
+        //    if (prices[i] >= prices[j])
+        //    {
+        //        i = j;
+        //        j++;
+        //    }
+        //    else if (prices[i] < prices[j])
+        //    {
+        //        if (prices[j] - prices[i] > result) result = prices[j] - prices[i];
+        //        j++;
+        //    }
+        //}
+        //return result;
     }
 }
