6bb17872080c9af238e8b3ea176ae44624bcd164
Adrian Yigit <bilalyigit594@gmail.com>
2024-02-28 18:34:50 +0300

    CharacterReplacement ; 28/02/2024

--- Algorithms.cs
+++ Algorithms.cs
@@ -1563,6 +1563,7 @@ public class Algorithms
     #endregion
     #region Medium
 
+    #region Arrays
     #region CanJump
     //Recursive CanJump
     //First check the number in the given index
@@ -1744,6 +1745,9 @@ public class Algorithms
         return Array.Empty<int>();
     }
     #endregion
+    #endregion
+
+    #region SlidingWindow
     #region LengthOfLongestSubstring
     public static int LengthOfLongestSubstring(string s)
     {
@@ -1766,6 +1770,79 @@ public class Algorithms
         }
         return maxLength;
     }
+    #endregion
+    #region
+    public static bool CheckInclusion(string s1, string s2)
+    {
+        int l = 0, r = 0, i = 0;
+        Dictionary<char, int> s1CharacterFrequencies = new();
+        Dictionary<char, int> s2CharacterFrequencies = new();
+
+        foreach (char c in s1)
+        {
+            if (s1CharacterFrequencies.ContainsKey(c))
+                s1CharacterFrequencies[c]++;
+            else s1CharacterFrequencies[c] = 1;
+        }
+
+        for (r = 0; r < s2.Length; r++)
+        {
+            if (!s2CharacterFrequencies.ContainsKey(s2[r]))
+            {
+                s2CharacterFrequencies.Add(s2[r], 1);
+            }
+            else
+            {
+                s2CharacterFrequencies[s2[r]] += 1;
+            }
+            while (r - l + 1 > s1.Length)
+            {
+                s2CharacterFrequencies[s2[l]] -= 1;
+                l++;
+            }
+
+            if (r - l + 1 == s1.Length)
+            {
+                for (i = 0; i < s1.Length; i++)
+                {
+                    if (!s2CharacterFrequencies.ContainsKey(s1[i]) || s2CharacterFrequencies[s1[i]] != s1CharacterFrequencies[s1[i]])
+                    {
+                        break;
+                    }
+                }
+                if (i == s1.Length)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+        //Dictionary<char, int> s2CharacterFrequencies = new();
+        //foreach(char c in s2)
+        //{
+        //    if (s2CharacterFrequencies.ContainsKey(c))
+        //        s2CharacterFrequencies[c]++;
+        //    else s2CharacterFrequencies[c] = 1;
+        //}
+        //Dictionary<char, int> s1CharacterFrequencies = new();
+        //foreach (char c in s1)
+        //{
+        //    if (s1CharacterFrequencies.ContainsKey(c))
+        //        s1CharacterFrequencies[c]++;
+        //    else s1CharacterFrequencies[c] = 1;
+        //}
+
+        //foreach(char c in s1CharacterFrequencies.Keys)
+        //{
+        //    if (s2CharacterFrequencies.ContainsKey(c) && s2CharacterFrequencies[c] == s1CharacterFrequencies[c])
+        //        continue;
+        //    else
+        //        return false;
+        //}
+        //return true;
+    }
+    #endregion
+
     #endregion
     #endregion
     #endregion
--- Program.cs
+++ Program.cs
@@ -4,77 +4,27 @@ public class Codewars
 {
     public static void Main(string[] args)
     {
-        string s1 = "ab";
-        string s2 = "eidboaoo";
-        CheckInclusion(s1, s2);
     }
-    public static bool CheckInclusion(string s1, string s2)
+    public static int CharacterReplacement(string s, int k)
     {
-        int l = 0, r = 0, i = 0;
-        Dictionary<char, int> s1CharacterFrequencies = new();
-        Dictionary<char, int> s2CharacterFrequencies = new();
+        Dictionary<char, int> charFrequencies = new();
+        int result = 0;
 
-        foreach (char c in s1)
+        int left = 0;
+        int maxFrequency = 0;
+        for (int right = 0; right < s.Length; right++)
         {
-            if (s1CharacterFrequencies.ContainsKey(c))
-                s1CharacterFrequencies[c]++;
-            else s1CharacterFrequencies[c] = 1;
+            charFrequencies[s[right]] = 1 + (charFrequencies.ContainsKey(s[right]) ? charFrequencies[s[right]] : 0);
+            maxFrequency = Math.Max(maxFrequency, charFrequencies[s[right]]);
+
+            while ((right - left + 1) - maxFrequency > k)
+            {
+                charFrequencies[s[left]] -= 1;
+                left += 1;
+            }
+
+            result = Math.Max(result, right - left + 1);
         }
-
-        for (r = 0; r < s2.Length; r++)
-        {
-            if (!s2CharacterFrequencies.ContainsKey(s2[r]))
-            {
-                s2CharacterFrequencies.Add(s2[r], 1);
-            }
-            else
-            {
-                s2CharacterFrequencies[s2[r]] += 1;
-            }
-            while (r - l + 1 > s1.Length)
-            {
-                s2CharacterFrequencies[s2[l]] -= 1;
-                l++;
-            }
-
-            if (r - l + 1 == s1.Length)
-            {
-                for (i = 0; i < s1.Length; i++)
-                {
-                    if (!s2CharacterFrequencies.ContainsKey(s1[i]) || s2CharacterFrequencies[s1[i]] != s1CharacterFrequencies[s1[i]])
-                    {
-                        break;
-                    }
-                }
-                if (i == s1.Length)
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
-        //Dictionary<char, int> s2CharacterFrequencies = new();
-        //foreach(char c in s2)
-        //{
-        //    if (s2CharacterFrequencies.ContainsKey(c))
-        //        s2CharacterFrequencies[c]++;
-        //    else s2CharacterFrequencies[c] = 1;
-        //}
-        //Dictionary<char, int> s1CharacterFrequencies = new();
-        //foreach (char c in s1)
-        //{
-        //    if (s1CharacterFrequencies.ContainsKey(c))
-        //        s1CharacterFrequencies[c]++;
-        //    else s1CharacterFrequencies[c] = 1;
-        //}
-
-        //foreach(char c in s1CharacterFrequencies.Keys)
-        //{
-        //    if (s2CharacterFrequencies.ContainsKey(c) && s2CharacterFrequencies[c] == s1CharacterFrequencies[c])
-        //        continue;
-        //    else
-        //        return false;
-        //}
-        //return true;
+        return result;
     }
 }
