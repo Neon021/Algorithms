1c78eb5b7dedaadccfa38de488118ddd77f2f25a
Adrian Yigit <bilalyigit594@gmail.com>
2024-03-01 11:33:48 +0300

    MaximumOddBinaryNumber questoin of the day ; 01/03/2024

--- Program.cs
+++ Program.cs
@@ -1,30 +1,27 @@
-﻿using System.Collections;
+﻿using System.Text;
 
 public class Codewars
 {
     public static void Main(string[] args)
     {
+        MaximumOddBinaryNumber("010010101010100");
     }
-    public static int CharacterReplacement(string s, int k)
+    public static string MaximumOddBinaryNumber(string s)
     {
-        Dictionary<char, int> charFrequencies = new();
-        int result = 0;
-
-        int left = 0;
-        int maxFrequency = 0;
-        for (int right = 0; right < s.Length; right++)
-        {
-            charFrequencies[s[right]] = 1 + (charFrequencies.ContainsKey(s[right]) ? charFrequencies[s[right]] : 0);
-            maxFrequency = Math.Max(maxFrequency, charFrequencies[s[right]]);
-
-            while ((right - left + 1) - maxFrequency > k)
-            {
-                charFrequencies[s[left]] -= 1;
-                left += 1;
-            }
-
-            result = Math.Max(result, right - left + 1);
-        }
-        return result;
+        //int numberOfOne = s.Count(x => x == '1');
+        //StringBuilder sb = new();
+        //for (int i = 0; i < numberOfOne - 1; i++)
+        //{
+        //    sb.Append('1');
+        //}
+        //for (int i = 0; i < s.Length - numberOfOne; i++)
+        //{
+        //    sb.Append('0');
+        //}
+        //sb.Append('1');
+        //return sb.ToString();
+        int count = s.Count(x => x == '1');
+        String? rs = new string('1', count - 1) + new string('0', s.Length - count) + new string('1', 1);
+        return rs;
     }
 }
