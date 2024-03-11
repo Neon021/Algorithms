2de5cf23b2252a2d03b08756e89e109f1f37efed
Adrian Yigit <bilalyigit594@gmail.com>
2024-03-11 16:08:29 +0300

    BinarySearch dsa-study week3 start ; 11/03/2024

--- Program.cs
+++ Program.cs
@@ -2,82 +2,20 @@
 {
     public static void Main(string[] args)
     {
-        // Example usage
-        int vertexCount = 6;
-        WeightedAdjacencyList graph = new(vertexCount);
-
-        // Add edges to the graph
-        graph.Edges[0].Add(new GraphEdge { To = 1, Weight = 1 });
-        graph.Edges[1].Add(new GraphEdge { To = 2, Weight = 2 });
-        graph.Edges[2].Add(new GraphEdge { To = 3, Weight = 3 });
-
-        graph.Edges[0].Add(new GraphEdge { To = 4, Weight = 4 });
-        graph.Edges[4].Add(new GraphEdge { To = 5, Weight = 5 });
-
-        int source = 0;
-        int needle = 3;
-
-        int[]? result = DFS(graph, source, needle);
-
-        if (result != null)
-        {
-            Console.WriteLine("Path found: " + string.Join(" -> ", result));
-        }
-        else
-        {
-            Console.WriteLine("Path not found.");
-        }
     }
-    public class GraphEdge
+    public int Search(int[] nums, int target)
     {
-        public int To { get; set; }
-        public int Weight { get; set; }
-    }
-
-    public class WeightedAdjacencyList
-    {
-        public List<GraphEdge>[] Edges { get; set; }
-
-        public WeightedAdjacencyList(int vertexCount)
+        int left = 0, right = nums.Length - 1;
+        while (left <= right)
         {
-            Edges = new List<GraphEdge>[vertexCount];
-            for (int i = 0; i < vertexCount; i++)
-            {
-                Edges[i] = new List<GraphEdge>();
-            }
+            int index = (left + right) / 2;
+            if (target > nums[index])
+                left = index + 1;
+            else if (target < nums[index])
+                right = index - 1;
+            else
+                return index;
         }
-    }
-    private static bool Walk(WeightedAdjacencyList graph, int curr, int needle, List<bool> seen, List<int> path)
-    {
-        if (seen[curr]) return false;
-
-        seen[curr] = true;
-
-        //recurse
-        //pre
-        path.Add(curr);
-        if (curr == needle) return true;
-
-        //recurse
-        List<GraphEdge> list = graph.Edges[curr];
-        for (int i = 0; i < list.Count; i++)
-        {
-            GraphEdge edge = list[i];
-            if (Walk(graph, edge.To, needle, seen, path)) return true;
-        }
-
-        //post
-        path.RemoveAt(path.Count - 1);
-
-        return false;
-    }
-    public static int[]? DFS(WeightedAdjacencyList graph, int source, int needle)
-    {
-        List<bool> seen = new(Enumerable.Repeat(false, graph.Edges.Length));
-        List<int> path = new();
-
-        Walk(graph, source, needle, seen, path);
-
-        return path.Count == 0 ? null : path.ToArray();
+        return -1;
     }
 }
