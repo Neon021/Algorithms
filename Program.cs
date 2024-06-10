f72ccc4b5e8a03c66cf573ae49bf365b4cc5dfcd
Adrian Yigit <bilalyigit594@gmail.com>
2024-06-10 17:24:03 +0300

    HeightChecker; 10/06/24

--- Program.cs
+++ Program.cs
@@ -5,26 +5,18 @@ public class Codewars
     static void Main(string[] args)
     {
     }
-    public double MyPow(double x, int n)
+    public int HeightChecker(int[] heights)
     {
-        long N = n;
-
-        if (N < 0)
+        int[] expected = new int[heights.Length];
+        Array.Copy(heights, expected, heights.Length);
+        Array.Sort(expected);
+        int res = 0;
+        for (int i = 0; i < heights.Length; i++)
         {
-            x = 1 / x;
-            N = -N;
+            if (heights[i] != expected[i])
+                res++;
         }
-        double result = 1.0;
-        while (N > 0)
-        {
-            if ((N % 2) == 1)
-            {
-                result *= x;
-            }
-
-            x *= x;
-            N /= 2;
-        }
-        return result;
+        return res;
     }
+
 }
