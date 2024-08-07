97b0842fe70abc1b4d7c3b6395bde7895682316a
Adrian Yigit <bilalyigit594@gmail.com>
2024-08-07 14:54:02 +0300

    TopKFrequentElements memory efficient high run cost implementation ; 07/08/24

--- Algorithms.cs
+++ Algorithms.cs
@@ -3101,6 +3101,68 @@ public class Algorithms
     #region Medium
 
     #region GroupAnagrams
+    public static List<List<string>> GroupAnaagrams(string[] strs)
+    {
+        if (strs.Length == 1)
+        {
+            return new() { new() { strs[0] } };
+        }
+
+        bool b = false;
+        Dictionary<string, List<string>> kv = new() { { strs[0], new() { strs[0] } } };
+
+        for (int i = 1; i < strs.Length; i++)
+        {
+            foreach (string str in kv.Keys)
+            {
+                if (IsAnagram(strs[i], str))
+                {
+                    kv[str].Add(strs[i]);
+                    b = true;
+                    break;
+                }
+                b = false;
+            }
+            if (!b)
+                kv.Add(strs[i], new() { strs[i] });
+        }
+
+        List<List<string>> res = new();
+        foreach (List<string> list in kv.Values)
+        {
+            res.Add(list);
+        }
+
+        return res;
+
+        bool IsAnagram(string s, string t)
+        {
+            int[] count1 = new int[256];
+            int[] count2 = new int[256];
+            int i;
+
+            for (i = 0; i < s.Length && i < t.Length; i++)
+            {
+                count1[s[i]]++;
+                count2[t[i]]++;
+            }
+
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            for (i = 0; i < 256; i++)
+            {
+                if (count1[i] != count2[i])
+                    return false;
+
+            }
+            return true;
+        }
+    }
+    #endregion
+    #region TopKFrequent
 
     #endregion
 
--- Program.cs
+++ Program.cs
@@ -1,70 +1,25 @@
-﻿using System.Reflection.Metadata.Ecma335;
-
-public class Codewars
+﻿public class Codewars
 {
     public static void Main(string[] args)
     {
-        string[] strs = new string[] { "act", "pots", "tops", "cat", "stop", "hat" };
-        GroupAnagrams(strs);
+        int[] ints = { 1, 2, 2, 3, 3, 3 };
+        TopKFrequent(ints, 2);
     }
-    public static List<List<string>> GroupAnagrams(string[] strs)
+    public static int[] TopKFrequent(int[] nums, int k)
     {
-        if (strs.Length == 1)
+        Dictionary<int, int> map = new();
+
+        foreach (int i in nums)
         {
-            return new() { new() { strs[0] } };
+            if (map.ContainsKey(i))
+                map[i]++;
+            else
+                map[i] = 1;
         }
 
-        bool b = false;
-        Dictionary<string, List<string>> kv = new() { { strs[0], new() { strs[0] } } };
 
-        for (int i = 1; i < strs.Length; i++)
-        {
-            foreach (string str in kv.Keys)
-            {
-                if (IsAnagram(strs[i], str))
-                {
-                    kv[str].Add(strs[i]);
-                    b = true; 
-                    break;
-                }
-                b = false;
-            }
-            if (!b)
-                kv.Add(strs[i], new() { strs[i] });
-        }
-
-        List<List<string>> res = new();
-        foreach (List<string> list in kv.Values)
-        {
-            res.Add(list);
-        }
+        var res = map.OrderByDescending(x => x.Value).Take(k).Select(x => x.Key).ToArray();
 
         return res;
-
-        bool IsAnagram(string s, string t)
-        {
-            int[] count1 = new int[256];
-            int[] count2 = new int[256];
-            int i;
-
-            for (i = 0; i < s.Length && i < t.Length; i++)
-            {
-                count1[s[i]]++;
-                count2[t[i]]++;
-            }
-
-            if (s.Length != t.Length)
-            {
-                return false;
-            }
-
-            for (i = 0; i < 256; i++)
-            {
-                if (count1[i] != count2[i])
-                    return false;
-
-            }
-            return true;
-        }
     }
 }
