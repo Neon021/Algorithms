9c889b2cefa30e0b089470e85bcc688686ad13a3
Adrian Yigit <bilalyigit594@gmail.com>
2024-08-16 10:28:46 +0300

    IsValidSudoku implemented ; 15/08/24 (FORGOT TO COMMIT)

--- Algorithms.cs
+++ Algorithms.cs
@@ -3163,6 +3163,25 @@ public class Algorithms
     }
     #endregion
     #region TopKFrequent
+    public static int[] TopKFrequent(int[] nums, int k)
+    {
+        Dictionary<int, int> map = new();
+
+        foreach (int i in nums)
+        {
+            if (map.ContainsKey(i))
+                map[i]++;
+            else
+                map[i] = 1;
+        }
+
+
+        var res = map.OrderByDescending(x => x.Value).Take(k).Select(x => x.Key).ToArray();
+
+        return res;
+    }
+    #endregion
+    #region String Encode and Decode
 
     #endregion
 
--- Program.cs
+++ Program.cs
@@ -1,25 +1,59 @@
-﻿public class Codewars
+﻿using System.Reflection.Metadata.Ecma335;
+
+public class Codewars
 {
     public static void Main(string[] args)
     {
-        int[] ints = { 1, 2, 2, 3, 3, 3 };
-        TopKFrequent(ints, 2);
     }
-    public static int[] TopKFrequent(int[] nums, int k)
+    public bool IsValidSudoku(char[][] board)
     {
-        Dictionary<int, int> map = new();
-
-        foreach (int i in nums)
+        for(int rowIdx = 0; rowIdx < 9; rowIdx++)
         {
-            if (map.ContainsKey(i))
-                map[i]++;
-            else
-                map[i] = 1;
+            HashSet<char> rowSet = new();
+            for(int colIdx = 0;  colIdx < 9; colIdx++)
+            {
+                char val = board[rowIdx][colIdx];
+                if(val != '.')
+                {
+                    if (!rowSet.Add(val))
+                        return false;
+                }
+            }
+        }
+        for (int colIdx = 0; colIdx < 9; colIdx++)
+        {
+            HashSet<char> colSet = new();
+            for (int rowIdx = 0; rowIdx < 9; rowIdx++)
+            {
+                char val = board[rowIdx][colIdx];
+                if (val != '.')
+                {
+                    if (!colSet.Add(val))
+                        return false;
+                }
+            }
+        }
+        for (int boxRow = 0; boxRow < 3; boxRow++)
+        {
+            for (int boxCol = 0; boxCol < 3; boxCol++)
+            {
+                HashSet<char> squareSet = new();
+
+                for (int i = boxRow * 3; i < boxRow * 3 + 3; i++)
+                {
+                    for (int j = boxCol * 3; j < boxCol * 3 + 3; j++)
+                    {
+                        char current = board[i][j];
+                        if (current != '.')
+                        {
+                            if (!squareSet.Add(current))
+                                return false;
+                        }
+                    }
+                }
+            }
         }
 
-
-        var res = map.OrderByDescending(x => x.Value).Take(k).Select(x => x.Key).ToArray();
-
-        return res;
+        return true;
     }
 }
