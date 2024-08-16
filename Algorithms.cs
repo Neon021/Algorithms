10fab49b9a3e3ef7e04cdeb85433127bbd7fd0ac
Adrian Yigit <bilalyigit594@gmail.com>
2024-08-16 17:17:01 +0300

    LongestSequence implemented ; 16/08/24

--- Algorithms.cs
+++ Algorithms.cs
@@ -3184,7 +3184,59 @@ public class Algorithms
     #region String Encode and Decode
 
     #endregion
+    #region ValidSudoku
+    public bool IsValidSudoku(char[][] board)
+    {
+        for (int rowIdx = 0; rowIdx < 9; rowIdx++)
+        {
+            HashSet<char> rowSet = new();
+            for (int colIdx = 0; colIdx < 9; colIdx++)
+            {
+                char val = board[rowIdx][colIdx];
+                if (val != '.')
+                {
+                    if (!rowSet.Add(val))
+                        return false;
+                }
+            }
+        }
+        for (int colIdx = 0; colIdx < 9; colIdx++)
+        {
+            HashSet<char> colSet = new();
+            for (int rowIdx = 0; rowIdx < 9; rowIdx++)
+            {
+                char val = board[rowIdx][colIdx];
+                if (val != '.')
+                {
+                    if (!colSet.Add(val))
+                        return false;
+                }
+            }
+        }
+        for (int boxRow = 0; boxRow < 3; boxRow++)
+        {
+            for (int boxCol = 0; boxCol < 3; boxCol++)
+            {
+                HashSet<char> squareSet = new();
 
+                for (int i = boxRow * 3; i < boxRow * 3 + 3; i++)
+                {
+                    for (int j = boxCol * 3; j < boxCol * 3 + 3; j++)
+                    {
+                        char current = board[i][j];
+                        if (current != '.')
+                        {
+                            if (!squareSet.Add(current))
+                                return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+    #endregion
     #endregion
     #endregion
 
--- Program.cs
+++ Program.cs
@@ -5,55 +5,25 @@ public class Codewars
     public static void Main(string[] args)
     {
     }
-    public bool IsValidSudoku(char[][] board)
+    public int LongestConsecutive(int[] nums)
     {
-        for(int rowIdx = 0; rowIdx < 9; rowIdx++)
-        {
-            HashSet<char> rowSet = new();
-            for(int colIdx = 0;  colIdx < 9; colIdx++)
-            {
-                char val = board[rowIdx][colIdx];
-                if(val != '.')
-                {
-                    if (!rowSet.Add(val))
-                        return false;
-                }
-            }
-        }
-        for (int colIdx = 0; colIdx < 9; colIdx++)
-        {
-            HashSet<char> colSet = new();
-            for (int rowIdx = 0; rowIdx < 9; rowIdx++)
-            {
-                char val = board[rowIdx][colIdx];
-                if (val != '.')
-                {
-                    if (!colSet.Add(val))
-                        return false;
-                }
-            }
-        }
-        for (int boxRow = 0; boxRow < 3; boxRow++)
-        {
-            for (int boxCol = 0; boxCol < 3; boxCol++)
-            {
-                HashSet<char> squareSet = new();
+        HashSet<int> map = new(nums);
+        int longest = 0;
 
-                for (int i = boxRow * 3; i < boxRow * 3 + 3; i++)
+        foreach (int i in nums)
+        {
+            //To not start another sequence count for a number that is an element in a already calculated sequence
+            if (!map.Contains(i - 1))
+            {
+                int seqLength = 1;
+                while (map.Contains(i + seqLength))
                 {
-                    for (int j = boxCol * 3; j < boxCol * 3 + 3; j++)
-                    {
-                        char current = board[i][j];
-                        if (current != '.')
-                        {
-                            if (!squareSet.Add(current))
-                                return false;
-                        }
-                    }
+                    seqLength++;
                 }
+                longest = Math.Max(longest, seqLength);
             }
         }
 
-        return true;
+        return longest;
     }
 }
