473fa42c6a2cb1a7465c04103b3c2f6872f2f5df
Adrian Yigit <bilalyigit594@gmail.com>
2024-10-09 17:43:10 +0300

    Implemented CarFleet; 09/10/24

--- Algorithms.cs
+++ Algorithms.cs
@@ -3541,6 +3541,80 @@ public class Algorithms
         }
     }
     #endregion
+    #region DailyTemperatures
+    //public static int[] DailyTemperatures(int[] temperatures)
+    //{
+    //    int[] res = new int[temperatures.Length];
+
+    //    int index = 0;
+    //    while (index < temperatures.Length)
+    //    {
+    //        int currTemp = temperatures[index];
+    //        Stack<int> offsetStack = new(temperatures.Skip(index + 1).Reverse());
+    //        int dayCount = 0;
+
+    //        while (offsetStack.Count != 0)
+    //        {
+    //            int nextTemp = offsetStack.Pop();
+    //            //What about equal?
+    //            if (nextTemp > currTemp)
+    //            {
+    //                dayCount++;
+    //                res[index] = dayCount;
+    //                break;
+    //            }
+    //            else
+    //            {
+    //                if (offsetStack.Count == 0)
+    //                    dayCount = 0;
+    //                else
+    //                    dayCount++;
+    //                continue;
+    //            }
+    //        }
+
+    //        res[index] = dayCount;
+    //        index++;
+    //    }
+
+    //    return res;
+    //    //Fernando(Queue) is faster than you Stack.
+    //    //int[] res = new int[temperatures.Length];
+
+    //    //int index = 0;
+    //    //while (index < temperatures.Length)
+    //    //{
+    //    //    int currTemp = temperatures[index];
+    //    //    Queue<int> offsetStack = new(temperatures.Skip(index + 1));
+    //    //    int dayCount = 0;
+
+    //    //    while (offsetStack.Count != 0)
+    //    //    {
+    //    //        int nextTemp = offsetStack.Dequeue();
+    //    //        //What about equal?
+    //    //        if (nextTemp > currTemp)
+    //    //        {
+    //    //            dayCount++;
+    //    //            res[index] = dayCount;
+    //    //            break;
+    //    //        }
+    //    //        else
+    //    //        {
+    //    //            if (offsetStack.Count == 0)
+    //    //                dayCount = 0;
+    //    //            else
+    //    //                dayCount++;
+    //    //            continue;
+    //    //        }
+    //    //    }
+
+    //    //    res[index] = dayCount;
+    //    //    index++;
+    //    //}
+
+    //    //return res;
+    //}
+    #endregion
     #endregion
     #endregion
 
--- Program.cs
+++ Program.cs
@@ -1,78 +1,67 @@
-﻿using System.Diagnostics.Metrics;
-
-public class Codewars
+﻿public class Codewars
 {
     public static void Main(string[] args)
     {
     }
-    public static int[] DailyTemperatures(int[] temperatures)
+    public static int CarFleet(int target, int[] position, int[] speed)
     {
-        int[] res = new int[temperatures.Length];
+        //This is practically a linear equation problem.
+        //We should check if cars intersect before or at <target>
+        //If any of the cars time to reach is smaller or equal to another they become a fleet.
 
-        int index = 0;
-        while (index < temperatures.Length)
+        //Created a jagged array to store position and speed of cars
+        int n = position.Length;
+        double[][] carParams = new double[n][];
+        for (int i = 0; i < n; i++)
         {
-            int currTemp = temperatures[index];
-            Stack<int> offsetStack = new(temperatures.Skip(index + 1).Reverse());
-            int dayCount = 0;
+            carParams[i] = new double[] { position[i], speed[i] };
+        }
 
-            while (offsetStack.Count != 0)
-            {
-                int nextTemp = offsetStack.Pop();
-                //What about equal?
-                if (nextTemp > currTemp)
-                {
-                    dayCount++;
-                    res[index] = dayCount;
-                    break;
-                }
-                else
-                {
-                    if (offsetStack.Count == 0)
-                        dayCount = 0;
-                    else
-                        dayCount++;
-                    continue;
-                }
-            }
+        //Sort cars according to their position
+        carParams = carParams.OrderByDescending(arr => arr[0]).ToArray();
 
-            res[index] = dayCount;
-            index++;
+        int res = 0;
+        double[] timeToReach = new double[n];
+        for (int i = 0; i < n; i++)
+        {
+            //Calculate time to reach of each car
+            //by subtracting its position from target and diving it by its speed
+            //Ex: target = 10, position = 7, speed = 1 => timetoReach = 3
+            //Ex: target = 10, position = 4, speed = 3 => timetoReach = 2
+            timeToReach[i] = (target - carParams[i][0]) / carParams[i][1];
+
+            //If any previous car's time to reach is calculated AND
+            //Refer to the 3rd line in the beginning comments
+            if (i > 0 && timeToReach[i] <= timeToReach[i - 1])
+                timeToReach[i] = timeToReach[i - 1];
+            else
+                res++;
         }
 
         return res;
-        //Fernando(Queue) is faster than you Stack.
-        //int[] res = new int[temperatures.Length];
 
-        //int index = 0;
-        //while (index < temperatures.Length)
+        //I'm not sure as to why Dictionary implementation does not work
+        //I sense it has somehting to do with indexing
+        //int n = position.Length;
+        //Dictionary<int, double[]> carParamss = new();
+        //for (int i = 0; i < n; i++)
         //{
-        //    int currTemp = temperatures[index];
-        //    Queue<int> offsetStack = new(temperatures.Skip(index + 1));
-        //    int dayCount = 0;
+        //    carParamss[i] = new double[] { position[i], speed[i] };
+        //}
 
-        //    while (offsetStack.Count != 0)
-        //    {
-        //        int nextTemp = offsetStack.Dequeue();
-        //        //What about equal?
-        //        if (nextTemp > currTemp)
-        //        {
-        //            dayCount++;
-        //            res[index] = dayCount;
-        //            break;
-        //        }
-        //        else
-        //        {
-        //            if (offsetStack.Count == 0)
-        //                dayCount = 0;
-        //            else
-        //                dayCount++;
-        //            continue;
-        //        }
-        //    }
+        //carParamss = carParamss
+        //    .OrderByDescending(kv => kv.Value[0])
+        //    .ToDictionary(kv => kv.Key, kv => kv.Value);
 
-        //    res[index] = dayCount;
-        //    index++;
+        //int res = 0;
+        //double[] timeToReach = new double[n];
+        //for (int i = 0; i < n; i++)
+        //{
+        //    timeToReach[i] = (target - carParamss[i][0]) / carParamss[i][1];
+        //    if (i > 0 && timeToReach[i] <= timeToReach[i - 1])
+        //        timeToReach[i] = timeToReach[i - 1];
+        //    else
+        //        res++;
         //}
 
         //return res;
