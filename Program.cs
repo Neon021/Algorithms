36156dffa4923356a56fb6909efd6ba918a18e5e
Furkan Yigit <bilalyigit594@gmail.com>
2024-12-16 18:10:27 +0300

    [NEW STUDY PROGRAM]     LinkedLists:     Merge. Imperative implementation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿public class Program
+﻿using static Algorithms;
+
+public class Program
 {
     public static void Main()
     {
@@ -8,7 +10,7 @@
         Console.WriteLine("Test Case 3: One Node in Each List");
         var singleNodeA = new Node<int>(5);
         var singleNodeB = new Node<int>(10);
-        mergedList = MergeLinkedLists(singleNodeA, singleNodeB);
+        mergedList = MergeLinkedListsImperative(singleNodeA, singleNodeB);
         Console.WriteLine("Merged List:");
         PrintList(mergedList);
         Console.WriteLine();
@@ -17,7 +19,7 @@
         Console.WriteLine("Test Case 4: Multiple Nodes in Each List, No Overlap");
         var listA = new Node<int>(1) { Next = new Node<int>(3) { Next = new Node<int>(5) } };
         var listB = new Node<int>(6) { Next = new Node<int>(8) { Next = new Node<int>(10) } };
-        mergedList = MergeLinkedLists(listA, listB);
+        mergedList = MergeLinkedListsImperative(listA, listB);
         Console.WriteLine("Merged List:");
         PrintList(mergedList);
         Console.WriteLine();
@@ -26,7 +28,7 @@
         Console.WriteLine("Test Case 5: Multiple Nodes in Each List, Overlapping Values");
         var overlappingListA = new Node<int>(1) { Next = new Node<int>(3) { Next = new Node<int>(5) } };
         var overlappingListB = new Node<int>(2) { Next = new Node<int>(3) { Next = new Node<int>(6) } };
-        mergedList = MergeLinkedLists(overlappingListA, overlappingListB);
+        mergedList = MergeLinkedListsImperative(overlappingListA, overlappingListB);
         Console.WriteLine("Merged List:");
         PrintList(mergedList);
         Console.WriteLine();
@@ -35,7 +37,7 @@
         Console.WriteLine("Test Case 6: One List Is Subset of the Other");
         var subsetListA = new Node<int>(1) { Next = new Node<int>(2) { Next = new Node<int>(3) } };
         var subsetListB = new Node<int>(2) { Next = new Node<int>(3) { Next = new Node<int>(4) } };
-        mergedList = MergeLinkedLists(subsetListA, subsetListB);
+        mergedList = MergeLinkedListsImperative(subsetListA, subsetListB);
         Console.WriteLine("Merged List:");
         PrintList(mergedList);
         Console.WriteLine();
@@ -44,7 +46,7 @@
         Console.WriteLine("Test Case 7: Lists with Identical Elements");
         var identicalListA = new Node<int>(1) { Next = new Node<int>(2) { Next = new Node<int>(3) } };
         var identicalListB = new Node<int>(1) { Next = new Node<int>(2) { Next = new Node<int>(3) } };
-        mergedList = MergeLinkedLists(identicalListA, identicalListB);
+        mergedList = MergeLinkedListsImperative(identicalListA, identicalListB);
         Console.WriteLine("Merged List:");
         PrintList(mergedList);
         Console.WriteLine();
@@ -61,7 +63,7 @@
         Console.WriteLine("null");
     }
 
-    public static Node<int>? MergeLinkedLists(Node<int>? headA, Node<int>? headB)
+    public static Node<int>? MergeLinkedListsRecursive(Node<int>? headA, Node<int>? headB)
     {
         //Base case is, we arrived at the end of one of the lists.
         if (headA == null)
@@ -74,15 +76,66 @@
         if (headA.Data < headB.Data)
         {
             res = headA;
-            res.Next = MergeLinkedLists(headA.Next, headB);
+            res.Next = MergeLinkedListsRecursive(headA.Next, headB);
         }
         else
         {
             res = headB;
-            res.Next = MergeLinkedLists(headA, headB.Next);
+            res.Next = MergeLinkedListsRecursive(headA, headB.Next);
         }
 
         return res;
+    }
+    public static Node<int> MergeLinkedListsImperative(Node<int> headA, Node<int> headB)
+    {
+        //REMEMBER! THESE ARE POINTERS TO OBJECTS ON THE HEAP NOT SEPARATE ENTITIES!!!
+        Node<int> dummyNode = new(default);
+        Node<int> currNode = dummyNode;
+
+        while (headA != null && headB != null)
+        {
+            if(headA.Data < headB.Data)
+            {
+                currNode.Next = headA;
+                //This would create another object on the heap
+                //currNode.Next = new(headA.Data);
+                headA = headA.Next;
+            }
+            else
+            {
+                currNode.Next = headB;
+                headB = headB.Next;
+            }
+
+            currNode = currNode.Next;
+        }
+
+        currNode.Next = headA ?? headB;
+
+        return dummyNode.Next;
+    }
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        ListNode dummy = new();
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null)
+        {
+            if (list1.val < list2.val)
+            {
+                current.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+        current.next = list1 ?? list2;
+
+        return dummy.next;
     }
 }
 
