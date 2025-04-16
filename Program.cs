74c0f4f96f40c556514a9bc064a108ffb9aa7ec2
Furkan Yigit <bilalyigit594@gmail.com>
2025-04-16 18:17:17 +0300

    [NEW STUDY PROGRAM]             Tree:             Traversal: morris traversal algorithm implemented

--- Program.cs
+++ Program.cs
@@ -6,6 +6,40 @@
     {
         public static void Main()
         {
+            //Node root = new();
+            //Node leftMostLeaf = new()
+            //{
+            //    Data = 7,
+            //    InOrderSuccessor = root,
+            //};
+            //Node rightMostLeaf = new()
+            //{
+            //    Data = 7,
+            //    InOrderSuccessor = root,
+            //};
+            //Node leftMostLeafPredecessor = new Node
+            //{
+            //    Data = 11,
+            //    Right = rightMostLeaf,
+            //    Left = leftMostLeaf
+            //};
+
+            //root.Data = 5;
+            //root.Left = new Node
+            //{
+            //    Data = 4,
+            //    Left = leftMostLeafPredecessor,
+            //};
+            //root.Right = new Node
+            //{
+            //    Data = 8,
+            //    Left = new Node { Data = 13 },
+            //    Right = new Node
+            //    {
+            //        Data = 4,
+            //        Right = new Node { Data = 1 },
+            //    }
+            //};
             Node root = new()
             {
                 Data = 5,
@@ -32,6 +66,7 @@
             };
 
             BinaryTree tree = new();
+            tree.morris_travel(root);
         }
     }
 }
@@ -43,9 +78,44 @@ namespace Solution
         public int Data;
         public Node? Left;
         public Node? Right;
+        public Node? InOrderSuccessor;
     }
 
     public class BinaryTree
     {
+        public void morris_travel(Node root)
+        {
+            if (root == null) return;
+
+            Node? curr, prev;
+            curr = root;
+
+            while (curr != null)
+            {
+                if (curr.Left == null)
+                {
+                    Console.WriteLine(curr.Data);
+                    curr = curr.Right;
+                }
+                else
+                {
+                    prev = curr.Left;
+                    while (prev.Right != null && prev.Right != curr)
+                        prev = prev.Right;
+
+                    if (prev.Right == null)
+                    {
+                        prev.Right = curr;
+                        curr = curr.Left;
+                    }
+                    else
+                    {
+                        prev.Right = null;
+                        Console.WriteLine(curr.Data);
+                        curr = curr.Right;
+                    }
+                }
+            }
+        }
     }
 }
