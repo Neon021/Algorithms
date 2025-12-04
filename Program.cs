ca7267de0950daa83b74a881e303e9f932adcd32
Furkan Yigit <bilalyigit594@gmail.com>
2025-12-04 17:02:14 +0300

    [UPDATE]: Worked on a prior probelm I couldn't solve. Namely Db_linear in codewars about two-pointers

--- Program.cs
+++ Program.cs
@@ -1,37 +1,66 @@
 ﻿namespace Main
 {
-    using Solution;
-
     public class Program
     {
         public static void Main()
         {
+            Console.WriteLine(DblLinear(15));
             //Unbalanced tree
-            Node root = new(7)
-            {
-                Left = new Node(23)
-                {
-                    Left = new Node(11)
-                    {
-                        Left = new Node(5),
-                        Right = new Node(4),
-                    },
-                },
-                Right = new Node(3)
-                {
-                    Left = new Node(18),
-                    Right = new Node(21)
-                }
-            };
+            //Node root = new(7)
+            //{
+            //    Left = new Node(23)
+            //    {
+            //        Left = new Node(11)
+            //        {
+            //            Left = new Node(5),
+            //            Right = new Node(4),
+            //        },
+            //    },
+            //    Right = new Node(3)
+            //    {
+            //        Left = new Node(18),
+            //        Right = new Node(21)
+            //    }
+            //};
 
-            BinaryTree tree = new(7);
-            tree.insert_AVL(new Node(14), tree.Root);
-            tree.insert_AVL(new Node(5), tree.Root);
-            tree.BFS(tree.Root);
+            //BinaryTree tree = new(7);
+            //tree.insert_AVL(new Node(14), tree.Root);
+            //tree.insert_AVL(new Node(5), tree.Root);
+            //tree.BFS(tree.Root);
+        }
+        public static int DblLinear(int n)
+        {
+            int[] resultSet = new int[n + 1];
+            resultSet[0] = 1;
+
+            int p1 = 0;
+            int p2 = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                // Calculate the next two potential candidates based on our current pointers
+                int next2 = 2 * resultSet[p1] + 1;
+                int next3 = 3 * resultSet[p2] + 1;
+
+                if (next2 <= next3)
+                {
+                    resultSet[i] = next2;
+                    p1++; // We used the 2x+1 candidate, so move that pointer forward
+
+                    // Handle duplicates
+                    if (next2 == next3) p2++;
+                }
+                else
+                {
+                    resultSet[i] = next3;
+                    p2++; // We used the 3x+1 candidate
+                }
+            }
+
+            return resultSet[n];
         }
     }
 }
-
 namespace Solution
 {
     public class Node
