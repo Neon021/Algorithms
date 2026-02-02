c19ff09ae0f98b0750afdb141d82bb36363e219e
Furkan Yigit <bilalyigit594@gmail.com>
2026-02-02 17:29:55 +0300

    [OUTTALENT]: Nikita's List: Circular Queue

--- AlgoCourses/AlgoCourses.cs
+++ AlgoCourses/AlgoCourses.cs
@@ -4294,7 +4294,48 @@
                 return -1;
             }
         }
+        public class CustomStack
+        {
+            private int _length;
+            private readonly int[] _stack;
+            private readonly int _capacity;
 
+            public CustomStack(int maxSize)
+            {
+                _length = 0;
+                _capacity = maxSize;
+                _stack = new int[_capacity];
+
+            }
+
+            public void Push(int x)
+            {
+                if (_length == _capacity) return;
+
+                _stack[_length] = x;
+                _length++;
+            }
+
+            public int Pop()
+            {
+                if (_length == 0) return -1;
+
+                int poppedVal = _stack[_length - 1];
+                _length--;
+                return poppedVal;
+            }
+
+            public void Increment(int k, int val)
+            {
+                int incVal = k;
+                if (_length < k) incVal = _length;
+
+                for (int i = 0; i < incVal; i++)
+                {
+                    _stack[i] += val;
+                }
+            }
+        }
         #endregion
 
         #endregion
--- Program.cs
+++ Program.cs
@@ -4,47 +4,153 @@
     {
         public static void Main()
         {
+            //["MyCircularDeque", "insertLast", "insertLast", "insertLast", "insertLast", "deleteFront", "deleteFront", "deleteFront", "insertLast", "insertLast"]
+            //[[3], [1], [2], [3], [4], [], [], [], [4], [6]]
+            MyCircularDeque myCircularDeque = new MyCircularDeque(3);
+            myCircularDeque.InsertLast(1); // return true
+            myCircularDeque.InsertLast(2); // return true
+            myCircularDeque.InsertLast(3); // return true
+            myCircularDeque.InsertLast(4); // return false, the deque is full
+            myCircularDeque.DeleteFront();  // return true
+            myCircularDeque.DeleteFront();  // return true
+            myCircularDeque.DeleteFront();  // return true
+            myCircularDeque.InsertLast(4); // return true
+            myCircularDeque.InsertLast(6); // return true
         }
-        public class CustomStack
+        public class Node
         {
+            public int Value;
+            public Node Next;
+            public Node Previous;
+
+            public Node(int val)
+            {
+                Value = val;
+            }
+        }
+
+        public class MyCircularDeque
+        {
+            private Node _first;
+            private Node _last;
+
             private int _length;
-            private readonly int[] _stack;
-            private readonly int _capacity;
+            private int _capacity;
 
-            public CustomStack(int maxSize)
+            public MyCircularDeque(int k)
             {
+                _capacity = k;
                 _length = 0;
-                _capacity = maxSize;
-                _stack = new int[_capacity];
-
             }
 
-            public void Push(int x)
+            public bool InsertFront(int value)
             {
-                if (_length == _capacity) return;
+                if (_length == _capacity)
+                    return false;
 
-                _stack[_length] = x;
-                _length++;
-            }
+                Node newNode = new Node(value);
 
-            public int Pop()
-            {
-                if (_length == 0) return -1;
-
-                int poppedVal = _stack[_length - 1];
-                _length--;
-                return poppedVal;
-            }
-
-            public void Increment(int k, int val)
-            {
-                int incVal = k;
-                if (_length < k) incVal = _length;
-
-                for (int i = 0; i < incVal; i++)
+                if (_first == null)
                 {
-                    _stack[i] += val;
+                    _first = newNode;
+                    _last = _first;
                 }
+                else
+                {
+                    _first.Next = newNode;
+
+                    Node tmp = _first;
+                    _first = newNode;
+                    _first.Previous = tmp;
+                }
+
+                _length++;
+                return true;
+            }
+
+            public bool InsertLast(int value)
+            {
+                if (_length == _capacity)
+                    return false;
+
+                Node newNode = new Node(value);
+
+                if (_last == null)
+                {
+                    _first = newNode;
+                    _last = _first;
+                }
+                else
+                {
+                    _last.Previous = newNode;
+
+                    Node tmp = _last;
+                    _last = newNode;
+                    _last.Next = tmp;
+                }
+
+                _length++;
+                return true;
+            }
+
+            public bool DeleteFront()
+            {
+                if (_length == 0 || _first == null)
+                    return false;
+
+                if (_length == 1)
+                {
+                    _last = null;
+                    _first = null;
+                }
+                else
+                    _first = _first.Previous;
+
+                _length--;
+                return true;
+            }
+
+            public bool DeleteLast()
+            {
+                if (_length == 0 || _last == null)
+                    return false;
+
+                if (_length == 1)
+                {
+                    _last = null;
+                    _first = null;
+                }
+                else
+                    _last = _last.Next;
+
+                _length--;
+                return true;
+            }
+
+            public int GetFront()
+            {
+                if (_length == 0 || _first == null)
+                    return -1;
+
+                return _first.Value;
+            }
+
+            public int GetRear()
+            {
+                if (_length == 0 || _last == null)
+                    return -1;
+
+                return _last.Value;
+            }
+
+            public bool IsEmpty()
+            {
+                return _length == 0;
+            }
+
+            public bool IsFull()
+            {
+                return _length == _capacity;
             }
         }
     }
