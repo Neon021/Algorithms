049f593b7b38c121fc2384848de74d41eaac1c78
Furkan Yigit <bilalyigit594@gmail.com>
2026-02-02 16:02:33 +0300

    [OUTTALENT]: Nikita's List: Stack with Increment functionality

--- AlgoCourses/AlgoCourses.cs
+++ AlgoCourses/AlgoCourses.cs
@@ -4102,6 +4102,7 @@
         #endregion
 
         #region Outtalent
+        #region Arrays
         public static int RemoveDuplicates(int[] nums)
         {
             if (nums.Length == 0) return 0;
@@ -4202,6 +4203,99 @@
 
         //    return stack.Count == 0;
         //}
+        #endregion
+        #region Stack
+        public class MinStack
+        {
+            // Main Stack storage
+            private int[] _stack;
+            private int _capacity;
+            private int _length;
+
+            // Min value storage
+            private int[] _minStack;
+            private int _minStackCapacity;
+            private int _minStackLength;
+
+            public MinStack()
+            {
+                _length = 0;
+                _capacity = 10;
+                _stack = new int[_capacity];
+
+                _minStackLength = 0;
+                _minStackCapacity = 10;
+                _minStack = new int[_minStackCapacity];
+            }
+
+            public void Push(int val)
+            {
+                if (_length == _capacity)
+                {
+                    _capacity *= 2;
+                    int[] newStack = new int[_capacity];
+                    for (int i = 0; i < _stack.Length; i++)
+                    {
+                        newStack[i] = _stack[i];
+                    }
+                    _stack = newStack;
+                }
+                _stack[_length] = val;
+                _length++;
+
+
+                //Only push to minStack if new value is <= current min
+                if (!(_minStackLength == 0 || val <= _minStack[_minStackLength - 1])) return;
+
+                if (_minStackLength == _minStackCapacity)
+                {
+                    _minStackCapacity *= 2;
+                    int[] newMinStack = new int[_minStackCapacity];
+                    for (int i = 0; i < _minStack.Length; i++)
+                    {
+                        newMinStack[i] = _minStack[i];
+                    }
+                    _minStack = newMinStack;
+                }
+                _minStack[_minStackLength] = val;
+                _minStackLength++;
+            }
+
+            public void Pop()
+            {
+                if (_length <= 0) return;
+
+                int valBeingPopped = _stack[_length - 1];
+
+                // Only pop from minStack if the values match
+                if (_minStackLength > 0 && valBeingPopped == _minStack[_minStackLength - 1])
+                    _minStackLength--;
+
+                //Always pop from mainStack
+                _length--;
+
+                //By only decreasing the _length we actually don't replace actual values in the array.
+                //However, they are now considered "out of bounds" and will be overwritten on the next Push.
+            }
+
+            public int Top()
+            {
+                if (_length > 0)
+                    return _stack[_length - 1];
+
+                return -1;
+            }
+
+            public int GetMin()
+            {
+                if (_minStackLength > 0)
+                    return _minStack[_minStackLength - 1];
+
+                return -1;
+            }
+        }
+
+        #endregion
 
         #endregion
     }
--- Program.cs
+++ Program.cs
@@ -4,107 +4,48 @@
     {
         public static void Main()
         {
-            //["MinStack","push","push","push","getMin","pop","top","getMin"]
-            //[[],[-2],[0],[-3],[],[],[],[]]
-            MinStack minStack = new MinStack();
-            minStack.Push(-2);
-            minStack.Push(0);
-            minStack.Push(-3);
-            int param_4 = minStack.GetMin();
-            minStack.Pop();
-            int param_3 = minStack.Top();
-            int param_5 = minStack.GetMin();
         }
-        public class MinStack
+        public class CustomStack
         {
-            // Main Stack storage
-            private int[] _stack;
-            private int _capacity;
             private int _length;
+            private readonly int[] _stack;
+            private readonly int _capacity;
 
-            // Min value storage
-            private int[] _minStack;
-            private int _minStackCapacity;
-            private int _minStackLength;
-
-            public MinStack()
+            public CustomStack(int maxSize)
             {
                 _length = 0;
-                _capacity = 10;
+                _capacity = maxSize;
                 _stack = new int[_capacity];
 
-                _minStackLength = 0;
-                _minStackCapacity = 10;
-                _minStack = new int[_minStackCapacity];
             }
 
-            public void Push(int val)
+            public void Push(int x)
             {
-                if (_length == _capacity)
-                {
-                    _capacity *= 2;
-                    int[] newStack = new int[_capacity];
-                    for (int i = 0; i < _stack.Length; i++)
-                    {
-                        newStack[i] = _stack[i];
-                    }
-                    _stack = newStack;
-                }
-                _stack[_length] = val;
+                if (_length == _capacity) return;
+
+                _stack[_length] = x;
                 _length++;
-
-
-                //Only push to minStack if new value is <= current min
-                if (!(_minStackLength == 0 || val <= _minStack[_minStackLength - 1])) return;
-
-                if (_minStackLength == _minStackCapacity)
-                {
-                    _minStackCapacity *= 2;
-                    int[] newMinStack = new int[_minStackCapacity];
-                    for (int i = 0; i < _minStack.Length; i++)
-                    {
-                        newMinStack[i] = _minStack[i];
-                    }
-                    _minStack = newMinStack;
-                }
-                _minStack[_minStackLength] = val;
-                _minStackLength++;
             }
 
-            public void Pop()
+            public int Pop()
             {
-                if (_length <= 0) return;
+                if (_length == 0) return -1;
 
-                int valBeingPopped = _stack[_length - 1];
-
-                // Only pop from minStack if the values match
-                if (_minStackLength > 0 && valBeingPopped == _minStack[_minStackLength - 1])
-                    _minStackLength--;
-
-                //Always pop from mainStack
+                int poppedVal = _stack[_length - 1];
                 _length--;
-
-                //By only decreasing the _length we actually don't replace actual values in the array.
-                //However, they are now considered "out of bounds" and will be overwritten on the next Push.
+                return poppedVal;
             }
 
-            public int Top()
+            public void Increment(int k, int val)
             {
-                if (_length > 0)
-                    return _stack[_length - 1];
+                int incVal = k;
+                if (_length < k) incVal = _length;
 
-                return -1;
-            }
-
-            public int GetMin()
-            {
-                if (_minStackLength > 0)
-                    return _minStack[_minStackLength - 1];
-
-                return -1;
+                for (int i = 0; i < incVal; i++)
+                {
+                    _stack[i] += val;
+                }
             }
         }
-
-
     }
 }
