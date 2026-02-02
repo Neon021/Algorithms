3aaf7f7287d4024a61e9f68f28c20437a1bffd0f
Furkan Yigit <bilalyigit594@gmail.com>
2026-02-02 15:48:56 +0300

    [OUTTALENT]: Nikita's List: Min Stack CORRECT VERSION

--- Program.cs
+++ Program.cs
@@ -17,67 +17,91 @@
         }
         public class MinStack
         {
-
+            // Main Stack storage
             private int[] _stack;
             private int _capacity;
             private int _length;
+
+            // Min value storage
+            private int[] _minStack;
+            private int _minStackCapacity;
+            private int _minStackLength;
 
             public MinStack()
             {
                 _length = 0;
                 _capacity = 10;
                 _stack = new int[_capacity];
+
+                _minStackLength = 0;
+                _minStackCapacity = 10;
+                _minStack = new int[_minStackCapacity];
             }
 
             public void Push(int val)
             {
-                if (_length + 1 < _capacity)
+                if (_length == _capacity)
                 {
-                    _stack[_length] = val;
-                    _length++;
-                }
-                else
-                {
-                    int[] tmp = _stack;
                     _capacity *= 2;
-                    _stack = new int[_capacity];
-                    for (int i = 0; i < tmp.Length; i++)
+                    int[] newStack = new int[_capacity];
+                    for (int i = 0; i < _stack.Length; i++)
                     {
-                        _stack[i] = tmp[i];
+                        newStack[i] = _stack[i];
                     }
-                    _stack[_length] = val;
-                    _length++;
+                    _stack = newStack;
                 }
+                _stack[_length] = val;
+                _length++;
+
+
+                //Only push to minStack if new value is <= current min
+                if (!(_minStackLength == 0 || val <= _minStack[_minStackLength - 1])) return;
+
+                if (_minStackLength == _minStackCapacity)
+                {
+                    _minStackCapacity *= 2;
+                    int[] newMinStack = new int[_minStackCapacity];
+                    for (int i = 0; i < _minStack.Length; i++)
+                    {
+                        newMinStack[i] = _minStack[i];
+                    }
+                    _minStack = newMinStack;
+                }
+                _minStack[_minStackLength] = val;
+                _minStackLength++;
             }
 
             public void Pop()
             {
-                if (_length > 0)
-                {
-                    _stack[_length - 1] = -1;
-                    _length--;
-                }
+                if (_length <= 0) return;
+
+                int valBeingPopped = _stack[_length - 1];
+
+                // Only pop from minStack if the values match
+                if (_minStackLength > 0 && valBeingPopped == _minStack[_minStackLength - 1])
+                    _minStackLength--;
+
+                //Always pop from mainStack
+                _length--;
+
+                //By only decreasing the _length we actually don't replace actual values in the array.
+                //However, they are now considered "out of bounds" and will be overwritten on the next Push.
             }
 
             public int Top()
             {
                 if (_length > 0)
-                {
                     return _stack[_length - 1];
-                }
 
                 return -1;
             }
 
             public int GetMin()
             {
-                if (_length > 0)
-                {
-                    IEnumerable<int> slice = _stack.Take(_length);
-                    return slice.Min();
-                }
+                if (_minStackLength > 0)
+                    return _minStack[_minStackLength - 1];
 
-                return int.MaxValue;
+                return -1;
             }
         }
 
