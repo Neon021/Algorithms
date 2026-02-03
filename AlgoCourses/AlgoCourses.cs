b43e01770c08f535c2292411d8d6b2e174e05e55
Furkan Yigit <bilalyigit594@gmail.com>
2026-02-03 13:39:47 +0300

    [OUTTALENT]: Nikita's List: DeleteDuplicates

--- AlgoCourses/AlgoCourses.cs
+++ AlgoCourses/AlgoCourses.cs
@@ -4473,6 +4473,27 @@
                 return _length == _capacity;
             }
         }
+
+        //public List<int> prevSmaller(List<int> A)
+        //{
+        //    List<int> result = new();
+
+        //    Stack<int> stack = new();
+        //    foreach (int a in A)
+        //    {
+        //        while (stack.Count > 0 && stack.Peek() >= a)
+        //            stack.Pop();
+
+        //        if (stack.Peek() < a)
+        //            result.Add(stack.Peek());
+        //        else
+        //            result.Add(-1);
+
+        //        stack.Push(a);
+        //    }
+        //    return result;
+        //}
+
         #endregion
 
         #endregion
--- Program.cs
+++ Program.cs
@@ -1,28 +1,41 @@
-﻿namespace Main
+﻿
+namespace Main
 {
     public class Program
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
         }
-        public List<int> prevSmaller(List<int> A)
+
+        public class ListNode
         {
-            List<int> result = new();
-
-            Stack<int> stack = new();
-            foreach (int a in A)
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
             {
-                while (stack.Count > 0 && stack.Peek() >= a)
-                    stack.Pop();
-
-                if (stack.Peek() < a)
-                    result.Add(stack.Peek());
-                else
-                    result.Add(-1);
-
-                stack.Push(a);
+                this.val = val;
+                this.next = next;
             }
-            return result;
+        }
+
+        public class Solution
+        {
+            public ListNode? DeleteDuplicates(ListNode head)
+            {
+                if (head == null || head.next == null)
+                    return head;
+
+                ListNode tmp = head;
+                while (tmp != null && tmp.next != null)
+                {
+                    if (tmp.val == tmp.next.val)
+                        tmp.next = tmp.next.next;
+                    else
+                        tmp = tmp.next;
+                }
+
+                return head;
+            }
         }
     }
 }
