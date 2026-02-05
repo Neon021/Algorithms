dcbf892cf823c8e199e23e572ad26dbe92cf2c6b
Furkan Yigit <bilalyigit594@gmail.com>
2026-02-05 16:58:22 +0300

    [OUTTALENT]: Nikita's List: KSmallestPairs. Only works for small input size.

--- AlgoCourses/AlgoCourses.cs
+++ AlgoCourses/AlgoCourses.cs
@@ -4685,8 +4685,159 @@
 
         //    return ptrOfFirstList;
         //}
-        #endregion
 
+        public static ListNode InsertGreatestCommonDivisors(ListNode head)
+        {
+            ListNode dummy = new(-1)
+            {
+                next = head
+            };
+
+            while (head != null && head.next != null)
+            {
+                int gdc = GCD(head.val, head.next.val);
+                ListNode newHead = head.next;
+                ListNode newNode = new ListNode(gdc);
+                head.next = newNode;
+                newNode.next = newHead;
+                head = newHead;
+            }
+
+            return dummy.next;
+        }
+
+        public static int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int tmp = b;
+                b = a % b;
+                a = tmp;
+            }
+            return a;
+        }
+        #endregion
+        #region Heap
+        public class ListNode
+        {
+            public int val;
+            public ListNode? next;
+            public ListNode(int val = 0, ListNode? next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public static ListNode MergeKLists(ListNode[] lists)
+        {
+            MinHeap minHeap = new(lists.Length * 10);
+
+            for (int i = 0; i < lists.Length; i++)
+            {
+                ListNode? currNode = lists[i];
+                while (currNode != null)
+                {
+                    minHeap.Insert(currNode.val);
+                    currNode = currNode.next;
+                }
+            }
+
+            return minHeap.GetMergedList();
+        }
+
+        public class MinHeap
+        {
+            private int[] _heapArray;
+            private int _capacity;
+            private int _size;
+
+            public MinHeap(int capacity)
+            {
+                _capacity = capacity;
+                _heapArray = new int[_capacity];
+                _size = 0;
+            }
+
+            public static int Parent(int index) => (index - 1) / 2;
+            public static int Left(int index) => index * 2 + 1;
+            public static int Right(int index) => index * 2 + 2;
+            public static void Swap<T>(ref T left, ref T right)
+            {
+                T temp = left;
+                left = right;
+                right = temp;
+            }
+
+            public bool Insert(int key)
+            {
+                if (_size == _capacity)
+                {
+                    _capacity *= 2;
+                    Array.Resize(ref _heapArray, _capacity);
+                }
+                int index = _size;
+                _heapArray[index] = key;
+                _size++;
+                while (index != 0 && _heapArray[index] < _heapArray[Parent(index)])
+                {
+                    Swap(ref _heapArray[index], ref _heapArray[Parent(index)]);
+                    index = Parent(index);
+                }
+
+                return true;
+            }
+            public int ExtractMin()
+            {
+                if (_size <= 0) return int.MaxValue;
+
+                if (_size == 1)
+                {
+                    _size--;
+                    return _heapArray[_size];
+                }
+
+                int min = _heapArray[0];
+                _heapArray[0] = _heapArray[_size - 1];
+                _size--;
+
+                MinHeapify(0);
+                return min;
+            }
+            public void MinHeapify(int index)
+            {
+                int left = Left(index);
+                int right = Right(index);
+                int root = index;
+
+                if (left < _size && _heapArray[left] < _heapArray[root])
+                    root = left;
+                if (right < _size && _heapArray[right] < _heapArray[root])
+                    root = right;
+
+                if (root != index) // did we change the index?
+                {
+                    Swap(ref _heapArray[index], ref _heapArray[root]);
+                    MinHeapify(root);// redo heapify starting from new root
+                }
+            }
+            public ListNode GetMergedList()
+            {
+                ListNode dummy = new(0);
+                ListNode current = dummy;
+
+                while (_size > 0)
+                {
+                    int minVal = ExtractMin();
+
+                    current.next = new ListNode(minVal);
+                    current = current.next;
+                }
+
+                return dummy.next;
+            }
+        }
+        #endregion
         #endregion
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,159 +1,152 @@
 ﻿namespace Main
 {
-    public static void Main()
+    public class Program
     {
-
+        public static void Main()
+        {
+            int[] nums1 = new int[] { -15, 20, 45, 117, 223 };
+            int[] nums2 = new int[] { -15, 20, 45, 117, 223, 546, 663, 714, 749, 801 };
+            IList<IList<int>> res = Solution.KSmallestPairs(nums1, nums2, 2);
+            foreach(var list in res)
+            {
+                foreach(var item in list)
+                    Console.WriteLine(item);
+            }
+        }
     }
 
     public static class Solution
     {
-        public static ListNode InsertGreatestCommonDivisors(ListNode head)
+        public static IList<IList<int>> KSmallestPairs(int[] nums1, int[] nums2, int k)
         {
-            ListNode dummy = new(-1)
-            {
-                next = head
-            };
+            //nums1 and nums2 are sorted in ascending order
+            //we need pair of (nums1i, nums2i) with the smallest sums
+            //For that, we need to store the result of their sum in our MinHeap
+            //But at the end we need to return the pairs not the results
+            //We can use a dictionary to have pairs as the value and the sum as the key
+            //After we iterate over all possible elements in num one and num two
+            //we put the results in the minHeap
+            //Then we can extractMin from the heap and use it to access our dictionary to get the related pair.
 
-            while (head != null && head.next != null)
-            {
-                int gdc = GCD(head.val, head.next.val);
-                ListNode newHead = head.next;
-                ListNode newNode = new ListNode(gdc);
-                head.next = newNode;
-                newNode.next = newHead;
-                head = newHead;
-            }
+            MinHeap heap = new MinHeap(nums1.Length);
 
-            return dummy.next;
-        }
-
-        public static int GCD(int a, int b)
-        {
-            while (b != 0)
+            for (int i = 0; i < nums1.Length; i++)
             {
-                int tmp = b;
-                b = a % b;
-                a = tmp;
-            }
-            return a;
-        }
-        public class ListNode
-        {
-            public int val;
-            public ListNode? next;
-            public ListNode(int val = 0, ListNode? next = null)
-            {
-                this.val = val;
-                this.next = next;
-            }
-        }
-
-        public static ListNode MergeKLists(ListNode[] lists)
-        {
-            MinHeap minHeap = new(lists.Length * 10);
-
-            for (int i = 0; i < lists.Length; i++)
-            {
-                ListNode? currNode = lists[i];
-                while (currNode != null)
+                for (int j = 0; j < nums2.Length; j++)
                 {
-                    minHeap.Insert(currNode.val);
-                    currNode = currNode.next;
+                    int sum = nums1[i] + nums2[j];
+                    List<int> currPair = new() { nums1[i], nums2[j] };
+                    heap.Insert(sum, currPair);
                 }
             }
 
-            return minHeap.GetMergedList();
+            IList<IList<int>> result = heap.GetPairs(k);
+
+            return result;
         }
+
+
 
         public class MinHeap
         {
-            private int[] _heapArray;
-            private int _capacity;
+            private int[] _heap;
             private int _size;
+            private int _capacity;
+            private Dictionary<int, List<IList<int>>> _pairResults = new();
 
             public MinHeap(int capacity)
             {
                 _capacity = capacity;
-                _heapArray = new int[_capacity];
-                _size = 0;
+                _heap = new int[_capacity];
             }
 
-            public static int Parent(int index) => (index - 1) / 2;
-            public static int Left(int index) => index * 2 + 1;
-            public static int Right(int index) => index * 2 + 2;
-            public static void Swap<T>(ref T left, ref T right)
+            private int Parent(int index) => (index - 1) / 2;
+            private int Left(int index) => (index * 2) + 1;
+            private int Right(int index) => (index * 2) + 2;
+            private void Swap(ref int left, ref int right)
             {
-                T temp = left;
+                int tmp = left;
                 left = right;
-                right = temp;
+                right = tmp;
             }
 
-            public bool Insert(int key)
+            public void Insert(int value, List<int> pair)
             {
-                if (_size == _capacity)
-                {
-                    _capacity *= 2;
-                    Array.Resize(ref _heapArray, _capacity);
-                }
+                if (_size == _capacity) return;
+
                 int index = _size;
-                _heapArray[index] = key;
+                int parentIndex = Parent(index);
+                _heap[index] = value;
                 _size++;
-                while (index != 0 && _heapArray[index] < _heapArray[Parent(index)])
+                _pairResults.TryGetValue(value, out List<IList<int>>? list);
+                if (list != null)
+                    list.Add(pair);
+                else
+                    _pairResults.TryAdd(value, new List<IList<int>>() { pair });
+                //bubble up
+                while (index != 0 && _heap[index] < _heap[parentIndex])
                 {
-                    Swap(ref _heapArray[index], ref _heapArray[Parent(index)]);
-                    index = Parent(index);
+                    Swap(ref _heap[index], ref _heap[parentIndex]);
+                    index = parentIndex;
+                    parentIndex = Parent(index);
+                }
+            }
+
+            public IList<IList<int>> GetPairs(int k)
+            {
+                IList<IList<int>> result = new List<IList<int>>();
+                while (_size >= 0 && k > 0)
+                {
+                    int smallest = ExtractMin();
+                    _pairResults.TryGetValue(smallest, out List<IList<int>>? pair);
+                    if (pair != null)
+                    {
+                        result.Add(pair[0]);
+                        pair.RemoveAt(0);
+                    }
+                    k--;
                 }
 
-                return true;
+                return result;
             }
-            public int ExtractMin()
+
+            private int ExtractMin()
             {
-                if (_size <= 0) return int.MaxValue;
+                if (_size <= 0)
+                    return int.MaxValue;
 
                 if (_size == 1)
                 {
                     _size--;
-                    return _heapArray[_size];
+                    return _heap[_size];
                 }
 
-                int min = _heapArray[0];
-                _heapArray[0] = _heapArray[_size - 1];
+                int min = _heap[0];
+                _heap[0] = _heap[_size - 1];
                 _size--;
 
                 MinHeapify(0);
+
                 return min;
             }
-            public void MinHeapify(int index)
+
+            private void MinHeapify(int index)
             {
                 int left = Left(index);
                 int right = Right(index);
                 int root = index;
 
-                if (left < _size && _heapArray[left] < _heapArray[root])
+                if (left < _size && _heap[left] < _heap[root])
                     root = left;
-                if (right < _size && _heapArray[right] < _heapArray[root])
+                if (right < _size && _heap[right] < _heap[root])
                     root = right;
 
-                if (root != index) // did we change the index?
+                if (root != index)//if we actually changed our root
                 {
-                    Swap(ref _heapArray[index], ref _heapArray[root]);
-                    MinHeapify(root);// redo heapify starting from new root
+                    Swap(ref _heap[index], ref _heap[root]);
+                    index = root;
+                    MinHeapify(index);
                 }
-            }
-            public ListNode GetMergedList()
-            {
-                ListNode dummy = new(0);
-                ListNode current = dummy;
-
-                while (_size > 0)
-                {
-                    int minVal = ExtractMin();
-
-                    current.next = new ListNode(minVal);
-                    current = current.next;
-                }
-
-                return dummy.next;
             }
         }
     }
