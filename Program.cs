17ccfee7b7659ae152c41e916c946362a3b08d8a
Furkan Yigit <bilalyigit594@gmail.com>
2026-03-28 16:10:28 +0300

    MaxConsecutive question solved

--- Program.cs
+++ Program.cs
@@ -4,10 +4,34 @@
     {
         public static void Main()
         {
+            Console.WriteLine(Solution.MaxConsecutive(new int[] { 1, 0, 1, 0}));
         }
     }
 
-    public class Solution
+    public static class Solution
     {
+        public static int MaxConsecutive(int[] nums)
+        {
+            int result = 0;
+            int left = 0;
+            int zeroCount = 0;
+
+            for (int right = 0; right < nums.Length; right++)
+            {
+                if (nums[right] == 0)
+                    zeroCount++;
+
+                while (zeroCount > 1)
+                {
+                    if (nums[left] == 0)
+                        zeroCount--;
+                    left++;
+                }
+
+                result = Math.Max(result, right - left + zeroCount);
+            }
+
+            return result;
+        }
     }
 }
