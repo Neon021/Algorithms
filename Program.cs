0fc517cdefb964acb1cfa6939b418420a575d5b1
Furkan Yigit <bilalyigit594@gmail.com>
2026-04-21 17:11:28 +0300

    .

--- Program.cs
+++ Program.cs
@@ -4,12 +4,46 @@
     {
         public static void Main()
         {
-            Console.WriteLine(Solution.MaxConsecutive(new int[] { 1, 0, 1, 0}));
         }
     }
 
     public static class Solution
     {
+        public static IList<IList<string>> Partition(string s)
+        {
+            List<IList<string>> result = new();
+            Backtracking(s, 0, new List<string>(), result);
+            return result;
+        }
+
+        private static void Backtracking(string s, int start, List<string> currPartition, List<IList<string>> result)
+        {
+            if (start == s.Length)
+            {
+                result.Add(currPartition);
+                return;
+            }
+
+            for (int end = start; end < s.Length; end++)
+            {
+                if (IsPalindrome(start, end))
+                    currPartition.Add(s[start..end]);
+
+                Backtracking(s, end + 1, currPartition, result);
+            }
+
+            bool IsPalindrome(int start, int end)
+            {
+                while (start < end && end > start) 
+                {
+                    if (s[start] != s[end]) return false;
+                    start++;
+                    end--;
+                }
+
+                return true;
+            }
+        }
         public static int MaxConsecutive(int[] nums)
         {
             int result = 0;
